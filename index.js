3d280f3e26455c78ed0f96e7c3079535e6e7e499
Henry <henry@HenrydeMacBook-Pro.local>
2021-01-22 23:05:39 +0800

    feat: 简单路由

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ const Koa = require("koa");
 const app = new Koa();
 
 app.use((ctx) => {
-	ctx.body = "hello world";
+	if (ctx.url === "/hello") {
+		// 处理不同的 URL
+		ctx.body = "hello world";
+	} else if (ctx.url.match(/\/users/)) {
+		// 解析 URL 上的参数
+		ctx.body = "users";
+	} else if (ctx.method === "GET") {
+		// 处理不同的 HTTP 方法
+		ctx.body = "get";
+	} else if (ctx.method === "POST") {
+		ctx.body = "post";
+	} else {
+		ctx.status = 404;
+	}
 });
 
 app.listen(3000);
