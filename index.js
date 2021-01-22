1aa33567b89e0f54b8fcf520d3638616a172f771
Henry <henry@HenrydeMacBook-Pro.local>
2021-01-22 23:56:49 +0800

    feat: 使用 koa-router 创建路由

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const Koa = require("koa");
+const Router = require("koa-router");
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx) => {
-	if (ctx.url === "/hello") {
-		// 处理不同的 URL
-		ctx.body = "hello world";
-	} else if (ctx.url.match(/\/users/)) {
-		// 解析 URL 上的参数
-		ctx.body = "users";
-	} else if (ctx.method === "GET") {
-		// 处理不同的 HTTP 方法
-		ctx.body = "get";
-	} else if (ctx.method === "POST") {
-		ctx.body = "post";
-	} else {
-		ctx.status = 404;
-	}
+// 使用 prefix 精简写法
+const userRouter = new Router({
+	prefix: "/users",
 });
+
+// 处理不同的 URL
+userRouter.get("/", (ctx) => {
+	ctx.body = "users";
+});
+
+// 解析 URL 上的参数
+userRouter.get("/:id", (ctx) => {
+	ctx.body = ctx.params.id;
+});
+
+// 处理不同的 HTTP 方法
+router.get("/", (ctx) => {
+	ctx.body = "get";
+});
+router.post("/", (ctx) => {
+	ctx.body = "post";
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+app.use(userRouter.routes()).use(router.allowedMethods());
 
 app.listen(3000);
