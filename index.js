dddac7102185e73d201d7fb1f45fd179b38d9e23
HenryC-3 <HenryHe0074@foxmail.com>
2021-01-23 22:33:12 +0800

    feat: 使用 koa-json-error 处理错误
    规定开发环境下显示错误堆栈,生产环境下禁用错误堆栈

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const Koa = require("koa");
 const KoaBody = require("koa-body"); // Koa 本身并不支持解析 request.body
+const error = require("koa-json-error");
 const app = new Koa();
 const route = require("./app/routes");
+require("dotenv").config();
 
 // 注意中间件的调用顺序
-// 错误处理
-app.use(async (ctx, next) => {
-	try {
-		await next();
-		// d.b; // 模拟运行时错误
-	} catch (err) {
-		ctx.status = err.status || err.statusCode || 500;
-		ctx.body = {
-			message: err.message,
-		};
-	}
-});
+app.use(
+	error({
+		postFormat: (error, { stack, ...rest }) =>
+			process.env.NODE_ENV === "production" ? rest : { stack, ...rest },
+	})
+);
 app.use(KoaBody());
 route(app);
 
