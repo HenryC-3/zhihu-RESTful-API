40f236725171d34bb500e82007fb1811afdbe72b
HenryC-3 <HenryHe0074@foxmail.com>
2021-01-23 19:21:00 +0800

    feat: 简单错误处理

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app = new Koa();
 const route = require("./app/routes");
 
 // 注意中间件的调用顺序
+// 错误处理
+app.use(async (ctx, next) => {
+	try {
+		await next();
+		// d.b; // 模拟运行时错误
+	} catch (err) {
+		ctx.status = err.status || err.statusCode || 500;
+		ctx.body = {
+			message: err.message,
+		};
+	}
+});
 app.use(KoaBody());
 route(app);
 
