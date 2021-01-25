d71ca88b5638ddf80ac3d94f07bf3dbe55718586
HenryC-3 <HenryHe0074@foxmail.com>
2021-01-26 00:26:26 +0800

    feat: 响应包含真实数据库的增删改查

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,29 +1,41 @@
-// 模拟数据库
-const db = [{ name: "Jack" }, { name: "Jhon" }];
-
+const User = require("../models/user");
+const mongoose = require("mongoose");
 class userController {
-	find(ctx) {
-		ctx.body = db;
+	async find(ctx) {
+		ctx.body = await User.find();
 	}
-	create(ctx) {
-		ctx.verifyParams({
-			name: { type: "string", required: true },
-			id: "number",
-		});
-		db.push(ctx.request.body);
-		ctx.body = `新建用户${JSON.stringify(ctx.request.body)}`;
+	async create(ctx) {
+		ctx.verifyParams({ name: { type: "string", required: true } });
+		ctx.body = await new User(ctx.request.body).save();
 	}
-	findByID(ctx) {
-		ctx.body = db[ctx.params.id * 1];
+	async findByID(ctx) {
+		if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
+			ctx.throw(404, "用户不存在");
+		} else {
+			ctx.body = await User.findById(ctx.params.id);
+		}
 	}
-	update(ctx) {
-		let prevData = JSON.stringify(db[ctx.params.id * 1]);
-		db[ctx.params.id * 1] = ctx.request.body;
-		ctx.body = `修改 ${prevData} 为 ${JSON.stringify(ctx.request.body)}`;
+	async update(ctx) {
+		// 判断 id 是否有效 https://stackoverflow.com/a/25798043
+		if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
+			ctx.throw(404, "用户不存在");
+		} else {
+			const user = await User.findByIdAndUpdate(
+				ctx.params.id,
+				ctx.request.body
+			);
+			// koa 未设置 ctx.body 或 ctx.throw 时,默认响应为 404 https://github.com/koajs/koa/issues/905#issuecomment-280997965
+			ctx.body = user;
+		}
 	}
-	del(ctx) {
-		db.splice(ctx.params.id * 1, 1);
-		ctx.status = 204;
+	async del(ctx) {
+		// 判断 id 是否有效
+		if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
+			ctx.throw(404, "用户不存在");
+		} else {
+			await User.findByIdAndDelete(ctx.params.id);
+			ctx.throw(204);
+		}
 	}
 }
 
