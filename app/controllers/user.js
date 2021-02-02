244d1d890a3acf3884581a50255e6aed715a4801
HenryC-3 <HenryHe0074@foxmail.com>
2021-02-02 21:48:51 +0800

    docs: 增加 JSdoc 注释

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,13 +1,32 @@
 const User = require("../models/user");
 const mongoose = require("mongoose");
+
+/**
+ * @category Controller
+ */
 class userController {
+	/**
+	 * 查找所有用户
+	 * @async
+	 * @param {object} ctx koa context
+	 */
 	async find(ctx) {
 		ctx.body = await User.find();
 	}
+	/**
+	 * 创建用户
+	 * @async
+	 * @param {object} ctx koa context
+	 */
 	async create(ctx) {
 		ctx.verifyParams({ name: { type: "string", required: true } });
 		ctx.body = await new User(ctx.request.body).save();
 	}
+	/**
+	 * 查找指定 ID 用户
+	 * @async
+	 * @param {object} ctx koa context
+	 */
 	async findByID(ctx) {
 		if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
 			ctx.throw(404, "用户不存在");
@@ -15,19 +34,33 @@ class userController {
 			ctx.body = await User.findById(ctx.params.id);
 		}
 	}
+	/**
+	 * 修改用户
+	 * @async
+	 * @param {object} ctx koa context
+	 */
 	async update(ctx) {
 		// 判断 id 是否有效 https://stackoverflow.com/a/25798043
 		if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
 			ctx.throw(404, "用户不存在");
 		} else {
+			/**
+			 * @type {object}
+			 * @description koa 未设置 ctx.body 或 ctx.throw 时,默认响应为 404
+			 * @see https://github.com/koajs/koa/issues/905#issuecomment-280997965
+			 */
 			const user = await User.findByIdAndUpdate(
 				ctx.params.id,
 				ctx.request.body
 			);
-			// koa 未设置 ctx.body 或 ctx.throw 时,默认响应为 404 https://github.com/koajs/koa/issues/905#issuecomment-280997965
 			ctx.body = user;
 		}
 	}
+	/**
+	 * 删除指定 ID 用户
+	 * @async
+	 * @param {object} ctx koa context
+	 */
 	async del(ctx) {
 		// 判断 id 是否有效
 		if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
 
+/**
+ * @description 批量注册 routes 目录下的 koa-router 实例
+ * @param {object} app 接收 koa 实例
+ */
 module.exports = (app) => {
 	fs.readdirSync(__dirname).forEach((file) => {
 		if (file === "index.js") return;
