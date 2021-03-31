b35fae985dd34f1e3e5e9b7e38c197eb955b6db3
HenryC-3 <2324375439@qq.com>
2021-03-31 14:51:03 +0800

    chore: 增加注释

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -4,7 +4,7 @@ const jsonwebtoken = require("jsonwebtoken");
 const path = require("path");
 class userController {
 	async checkUser(ctx, next) {
-		// token 中的 id 必须与 URL 中的 id 相同, 确保登陆用户只能修改自身信息
+		// NOTE token 中的 id 必须与 URL 中的 id 相同, 确保登陆用户只能修改自身信息
 		if (ctx.state.user._id !== ctx.params.id) {
 			ctx.throw(403, "没有权限");
 		}
@@ -14,23 +14,23 @@ class userController {
 		ctx.body = await User.find();
 	}
 	async create(ctx) {
-		// 验证, 创建用户时, 必须有用户名和密码
+		// NOTE 创建用户时, 必须有用户名和密码
 		ctx.verifyParams({
 			name: { type: "string", required: true },
 			password: { type: "string", required: true },
 		});
 
-		// 假设不能出现相同用户名, 通过查询用户名验证用户是否存在
+		// NOTE 假设不能出现相同用户名, 通过查询用户名验证用户是否存在
 		if (await User.findOne(ctx.request.body)) {
 			ctx.throw(409, "用户已存在");
 		}
 		ctx.body = await new User(ctx.request.body).save();
 	}
 	async findByID(ctx) {
-		// url 格式 http://localhost:3000/users/60116cdb2bde543ca7350c2b?fields=locations;business
+		// NOTE url 格式 http://localhost:3000/users/60116cdb2bde543ca7350c2b?fields=locations;business
 		const { fields } = ctx.query;
 
-		// 处理获得的 url 参数, 使其满足 select 语法 https://mongoosejs.com/docs/api.html#query_Query-select
+		// NOTE 处理获得的 url 参数, 使其满足 select 语法 https://mongoosejs.com/docs/api.html#query_Query-select
 		const seletedFields = fields
 			.split(";")
 			.filter((f) => f) //过滤空参数
@@ -56,7 +56,7 @@ class userController {
 			employments: { type: "array", itemType: "object", required: false },
 			enducation: { type: "string", itemType: "object", required: false },
 		});
-		// 判断 id 是否有效 https://stackoverflow.com/a/25798043
+		// NOTE 判断 id 是否有效 https://stackoverflow.com/a/25798043
 		if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
 			ctx.throw(404, "用户不存在");
 		} else {
@@ -64,12 +64,12 @@ class userController {
 				ctx.params.id,
 				ctx.request.body
 			);
-			// koa 未设置 ctx.body 或 ctx.throw 时,默认响应为 404 https://github.com/koajs/koa/issues/905#issuecomment-280997965
+			// NOTE koa 未设置 ctx.body 或 ctx.throw 时,默认响应为 404 https://github.com/koajs/koa/issues/905#issuecomment-280997965
 			ctx.body = user;
 		}
 	}
 	async del(ctx) {
-		// 判断 id 是否有效
+		// NOTE 判断 id 是否有效
 		if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
 			ctx.throw(404, "用户不存在");
 		} else {
--- app/models/user.js
+++ app/models/user.js
@@ -5,13 +5,13 @@ const userSchema = new Schema({
 	__v: { type: Number, select: false },
 	name: { type: String, required: true },
 
-	// select:false 用于不在响应体中不显示该 field https://mongoosejs.com/docs/schematypes.html
+	// NOTE select:false 用于不在响应体中不显示该 field https://mongoosejs.com/docs/schematypes.html
 	password: { type: String, required: true, select: false },
 	avatar_url: { type: String },
 	gender: {
 		type: String,
 		required: true,
-		enum: ["male", "female"], // enum 用于校验字符 [Mongoose v5.11.14: Validation](https://mongoosejs.com/docs/validation.html)
+		enum: ["male", "female"], // NOTE enum 用于校验字符 [Mongoose v5.11.14: Validation](https://mongoosejs.com/docs/validation.html)
 		default: "male",
 	},
 	headline: { type: String },
@@ -39,8 +39,8 @@ const userSchema = new Schema({
 		select: false,
 	},
 	following: {
-		// [mongoose开发详解 - 拍岸的博客 | Paian's Blog](https://mobilesite.github.io/2017/05/07/how-to-use-mongoose/)
-		// ref 引用了 User document, 后续可以通过 ObjectId 查询 User 中对应 Id 用户的其他数据
+		// VIEW [mongoose开发详解 - 拍岸的博客 | Paian's Blog](https://mobilesite.github.io/2017/05/07/how-to-use-mongoose/)
+		// NOTE ref 引用了 User document, 后续可以通过 ObjectId 查询 User 中对应 Id 用户的其他数据
 		type: [{ type: Schema.Types.ObjectId, ref: "User" }],
 		select: false,
 	},
